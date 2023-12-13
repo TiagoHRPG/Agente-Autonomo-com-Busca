e3312a4d4cffef9240df834c42988f6fb1032c79
Tiago Henrique <tiagohrpg@gmail.com>
2023-12-13 20:31:23 -0300

    agent movement and dfs

--- sketch.js
+++ sketch.js
@@ -6,13 +6,30 @@ function setup() {
   
   
   world = new World(canva_width, canva_height, 72)
-  agent = new Agent(world.blockWidth, random(canva_width), random(canva_height))
+  agent = new Agent(world.blockWidth, int(random(world.res)), int(random(world.res)))
 }
+
+var time = 0;
+var execute = 0;
+
+var i = 0
 
 function draw() {
   background(220);
 
-  world.draw();
-  agent.draw();
 
+  if(millis() - time >= 2000){
+    if (i % 2 == 0) agent.move_unit(1, 0)
+    else agent.move_unit(0, 1)
+
+    time = millis()
+    i++
+  }
+  
+  
+  
+
+  world.draw();
+  agent.draw(world);
 }
+
