995b4e22ccf2836cb84b3fab68384a523cd4f75f
Tiago Henrique <tiagohrpg@gmail.com>
2023-12-15 17:19:36 -0300

    add agent movement

--- agent.js
+++ agent.js
@@ -6,10 +6,13 @@ function sleep(millisecondsDuration)
 }
 
 var time = 0;
+var i = 1;
 class Agent{
     constructor(headSize, x, y){
         this.headSize = headSize;
         this.pos = createVector(int(x), int(y)); // será o índice da matriz do mundo
+
+        this.path = []
     }
 
 
@@ -25,14 +28,37 @@ class Agent{
 
     }
 
-    execute_path(){
-        let i = 0
-        while (i < 4){
-            if (millis() - time >= 2000){
-                if ( i % 2 == 0) this.move_unit(1,0)
-                else this.move_unit(0, 1)
-                time = millis()
+    move(x, y){
+        x = int(x)
+        y = int(y)
+        if (abs(x - this.pos.x) > 1 || abs(y - this.pos.y) > 1){
+            print("Movimento inválido")
+        }
+
+        this.pos.x = x
+        this.pos.y = y
+
+        print(this.pos.x, this.pos.y)
+
+    }
+
+    cost2time(cost){
+        if (cost == 1) return 200
+        if (cost == 5) return 700
+        if (cost == 10) return 1200
+    }
+
+    execute_path(world){
+        if (i < this.path.length){
+            
+            let cost = world.costs[world.terrain[this.path[i-1][0]][this.path[i-1][1]]]
+
+            if (millis() - time >= this.cost2time(cost)){
+                let [pos_x, pos_y] = this.path[i]
+                this.move(pos_x, pos_y)
                 i += 1
+
+                time = millis()
             }
         }
         
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var time = 0;
 var execute = 0;
-// jkhidd 
+
 var i = 0
 var food;
 var func_search;
@@ -34,27 +34,49 @@ function setup() {
   dfs = new DFS(world.res)
   uniform = new Uniform(world.res)
   astar = new AStar(world.res)
+
+  search_method = astar
+
+  // trocar o nome das funções por search e chamar search_method.search
   func_bfs = bfs.bfs(world, new Node(agent.pos.x, agent.pos.y), food)
   func_dfs = dfs.dfs(world, new Node(agent.pos.x, agent.pos.y), food)
   func_uniform = uniform.uniform(world, new Node(agent.pos.x, agent.pos.y), food)
   func_astar = astar.astar(world, new Node(agent.pos.x, agent.pos.y), food)
-  func_search = func_bfs
+
+
+  func_search = func_astar
 }
+
+
 
 function draw() {
   frameRate(1000)
   background(220);
 
-  end = func_search.next()
+  let end = func_search.next()
+
+  //acontece uma vez
+  if (end.value == 1){
+    agent.path = search_method.path
+    print(agent.path[0])
+  }
+
+  // acontece sempre depois do fim da busca
+  if (end.done == 1){
+    agent.execute_path(world)
+  }
   world.draw();
 
+
+  draw_food()
+  agent.draw(world);
+  
+}
+
+function draw_food(){
   fill(160, 32, 240);
   stroke(255, 255, 255);
   let pos_x = (food.x + 1/2) * world.blockWidth
   let pos_y = (food.y + 1/2) * world.blockWidth
   ellipse(pos_x, pos_y, agent.headSize, agent.headSize); 
-
-  agent.draw(world);
-  
 }
-
